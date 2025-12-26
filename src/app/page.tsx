5acf3c248199f8be096b67e0b353a8ab3a35db74
Soham De Roy <sohamderoy@Sohams-MacBook-Pro.local>
2025-12-26 13:37:50 +0530

    minor change for linking /fapi-simulator to /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
+import { HOME_PAGE_PATH } from "@/utils/data/paths/paths.ui.constants";
 
 export default function RootPage() {
-  redirect("/home");
+  redirect(HOME_PAGE_PATH);
 }
--- src/components/lib/appName/index.tsx
+++ src/components/lib/appName/index.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
+import { HOME_PAGE_PATH } from "@/utils/data/paths/paths.ui.constants";
 import { AppNameProps } from "./types";
 
 const AppName = ({ style }: AppNameProps) => {
   return (
-    <h1
-      className="font-extrabold bg-gradient-to-r from-blue-500 to-indigo-500 bg-clip-text text-transparent font-googleSansFlex"
-      style={style}
-    >
-      FAPI
-    </h1>
+    <Link href={HOME_PAGE_PATH} className="cursor-pointer">
+      <h1
+        className="font-extrabold bg-gradient-to-r from-blue-500 to-indigo-500 bg-clip-text text-transparent font-googleSansFlex hover:from-blue-400 hover:to-indigo-400 transition-all"
+        style={style}
+      >
+        FAPI
+      </h1>
+    </Link>
   );
 };
 
